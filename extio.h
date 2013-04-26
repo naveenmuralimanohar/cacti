74d04f6461ac1faf1bdcb9706c8df80e895443b8
vaishnav.srinivas@gmail.com <vaishnav.srinivas@gmail.com@89e5a4c0-f7d9-2300-cf40-2de2ce65d21d>
2013-04-26 23:47:23 +0000

    cacti io files added

---
+++ extio.h
@@ -0,0 +1,30 @@
+#ifndef _extio_H_
+#define _extio_H_
+#include "parameter.h"
+#include "component.h"
+
+class Extio : public Component
+{
+public: 
+     
+    Extio(){}; 
+   //The power calculation 
+    	
+	int extio_area(double io_area); 
+
+	int extio_eye(double io_tmargin, double io_vmargin); 
+
+	int extio_power_dynamic(double io_power_dynamic); 
+
+	int extio_power_phy(double phy_power); 
+
+	int extio_power_term(double io_power_term); 
+
+ 
+int extio_area_timing(double io_power, double phy_power, double io_area, double io_tmargin, double io_vmargin); 
+
+ 
+}; 
+ 
+ 
+#endif  // _extio_H_ 
